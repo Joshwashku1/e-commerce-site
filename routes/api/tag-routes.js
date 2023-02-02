aa0fafd1b6ad3c7c1cf7ab8a5d18b533df66e8e3
JoshuaHamann <joshwashku@yahoo.com>
2023-02-01 22:59:27 -0600

    added functionality to grab tags by id with a get route

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -17,9 +17,17 @@ router.get('/', async (req, res) => {
   }
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+  try{
+    const tagById = await Tag.findByPk(req.params.id, {
+      include: [{model: Product}],
+    });
+    res.status(200).json(tagById);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.post('/', (req, res) => {
