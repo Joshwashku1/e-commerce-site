8e63cb9fd97a947e1b5d4a9389738ca9b03b8b59
JoshuaHamann <joshwashku@yahoo.com>
2023-02-01 23:09:01 -0600

    added functionality to delete a tag by id through its endpoint

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -38,8 +38,18 @@ router.put('/:id', (req, res) => {
   // update a tag's name by its `id` value
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete on tag by its `id` value
+  try{
+    const deletedTag = await Tag.destroy({
+        where: {
+          id: req.params.id
+        }
+    });
+    res.status(200).json(deletedTag);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
