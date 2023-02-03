a813b833abe0ac98d5fb26afaee806c37c59296f
JoshuaHamann <joshwashku@yahoo.com>
2023-02-02 18:47:56 -0600

    added the rest of the functionality for the endpoints

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -30,12 +30,29 @@ router.get('/:id', async (req, res) => {
   }
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new tag
+  try{
+    const newTag = await Tag.create(req.body);
+    console.log(req.body);
+    res.status(200).json(newTag);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a tag's name by its `id` value
+  try{
+    const updateTag = await Tag.update(req.body, {
+      where: {
+        id: req.params.id
+      }
+    });
+    res.status(200).json(updateTag);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.delete('/:id', async (req, res) => {
